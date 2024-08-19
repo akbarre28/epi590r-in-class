317d3c9a372a498290a77f7a873c41f34d0ca8f6
Alexis Barrett <akbarre@emory.edu>
2024-08-19 11:40:07 -0400

    Added clean data

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -3,7 +3,7 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
+setwd("/Users/akbarre/Desktop/Emory/FALL 24/EPI 590R/epi590r/data/raw/") #("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
